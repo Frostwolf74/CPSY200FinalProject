91d037d93b90d84d3a6e4da4522fa7929acb0a0b
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-04-14 22:32:33 -0600

    added database reader

--- CPSY200FinalProject/Components/Persistence/ReadDatabase.cs
+++ CPSY200FinalProject/Components/Persistence/ReadDatabase.cs
@@ -1,6 +1,99 @@
-﻿namespace CPSY200FinalProject.Components.Persistence;
+﻿using CPSY200FinalProject.Components.Layout;
+using MySqlConnector;
+
+namespace CPSY200FinalProject.Components.Persistence;
 
 public class ReadDatabase
 {
+    public static MySqlConnection databaseConfig()
+    {
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+        {
+            Server = "localhost",
+            Database = "CPSY200FinalProject",
+            UserID = "root",
+            Password = "password"
+        };
     
+        MySqlConnection connection = new MySqlConnection(builder.ToString());
+        connection.Open();
+
+        return connection;
+    }
+    
+    public static List<CustomerObj> readCustomers(MySqlConnection connection)
+    {
+        string query = 
+            "SELECT * FROM customer"
+        ;
+        
+        MySqlCommand command = new MySqlCommand(query, connection);
+        List<CustomerObj> customers = new();
+        
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string firstName = reader.GetString("firstName");
+                string lastName = reader.GetString("lastName");
+                int phoneNumber = reader.GetInt32("phone");
+                string email = reader.GetString("email");
+                string notes = reader.GetString("notes");
+                
+                customers.Add(new CustomerObj(firstName, lastName, email, notes, phoneNumber));
+            }
+        }
+        
+        return customers;
+    }
+    
+    public static List<EquipmentObj> readEquipment(MySqlConnection connection)
+    {
+        string query = 
+            "SELECT * FROM equipment"
+        ;
+        
+        MySqlCommand command = new MySqlCommand(query, connection);
+        List<EquipmentObj> equipment = new();
+        
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string category = reader.GetString("category");
+                string name = reader.GetString("name");
+                string description = reader.GetString("description");
+                double dailyRentalCost = reader.GetDouble("dailyRentalCost");
+                
+                equipment.Add(new EquipmentObj(category, name, description, dailyRentalCost));
+            }
+        }
+        
+        return equipment;
+    }
+    
+    public static List<RentalObj> readRentals(MySqlConnection connection)
+    {
+        string query = 
+            "SELECT * FROM rental"
+        ;
+        
+        MySqlCommand command = new MySqlCommand(query, connection);
+        List<RentalObj> rentals = new();
+        
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                string rentalDate = reader.GetString("rentalDate");
+                string returnDate = reader.GetString("returnDate");
+                double price = reader.GetDouble("price");
+                double totalCost = reader.GetDouble("totalCost");
+                
+                rentals.Add(new RentalObj(rentalDate, returnDate, price, totalCost));
+            }
+        }
+        
+        return rentals;
+    }
 }
--- CPSY200FinalProject/Components/Persistence/WriteDatabase.cs
+++ CPSY200FinalProject/Components/Persistence/WriteDatabase.cs
@@ -1,11 +1,11 @@
 ﻿using CPSY200FinalProject.Components.Layout;
+using MySqlConnector;
 
 namespace CPSY200FinalProject.Components.Persistence;
-using MySqlConnector;
 
 public class WriteDatabase
 {
-    public static void write(List<CustomerObj> customers)
+    public static MySqlConnection databaseConfig()
     {
         MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
         {
@@ -18,14 +18,93 @@ public class WriteDatabase
         MySqlConnection connection = new MySqlConnection(builder.ToString());
         connection.Open();
 
+        return connection;
+    }
+    
+    public static void writeCustomers(MySqlConnection connection, List<CustomerObj> customers)
+    {
+        string query0 =
+            "DROP TABLE IF EXISTS customer"
+        ;
+
+        string query1 =
+            "CREATE TABLE customer (customerID INT NOT NULL, firstName VARCHAR(255) NOT NULL, lastName VARCHAR(255) NOT NULL, phone INT NULL, email VARCHAR(255) NOT NULL, notes VARCHAR(255) NOT NULL);"
+        ;
+        
+        MySqlCommand command0 = new MySqlCommand(query0, connection);   
+        command0.ExecuteNonQuery();
+            
+        MySqlCommand command1 = new MySqlCommand(query1, connection);   
+        command1.ExecuteNonQuery();
+
+        int i = 0;
         foreach (var customer in customers)
         {
-            string query =
-                $"INSERT INTO customers VALUES('{customer.firstName}', '{customer.lastName}', {customer.phoneNumber}, '{customer.email}', '{customer.notes}')"
+            i++; 
+            string query2 =
+                $"INSERT INTO customer VALUES({i}, '{customer.firstName}', '{customer.lastName}', {customer.phoneNumber}, '{customer.email}', '{customer.notes}')"
             ;
+            
+            MySqlCommand command2 = new MySqlCommand(query2, connection);   
+            command2.ExecuteNonQuery();
+        }
+    }
+    
+    public static void writeEquipment(MySqlConnection connection, List<EquipmentObj> equipment)
+    {
+        string query0 =
+            "DROP TABLE IF EXISTS equipment"
+        ;
+
+        string query1 =
+            "CREATE TABLE equipment (equipmentID INT NOT NULL, category VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) NOT NULL, dailyRentalCost DOUBLE NOT NULL);"
+        ;
         
-            MySqlCommand command = new MySqlCommand(query, connection);   
-            command.ExecuteNonQuery();
+        MySqlCommand command0 = new MySqlCommand(query0, connection);   
+        command0.ExecuteNonQuery();
+            
+        MySqlCommand command1 = new MySqlCommand(query1, connection);   
+        command1.ExecuteNonQuery();
+
+        int i = 0;
+        foreach (var e in equipment)
+        {
+            i++;
+            string query2 =
+                $"INSERT INTO equipment VALUES({i}, '{e.category}', '{e.name}', '{e.description}', {e.dailyRentalCost})"
+            ;
+            
+            MySqlCommand command2 = new MySqlCommand(query2, connection);   
+            command2.ExecuteNonQuery();
+        }
+    }
+    
+    public static void writeRentals(MySqlConnection connection, List<RentalObj> rentals)
+    {
+        string query0 =
+            "DROP TABLE IF EXISTS rental"
+        ;
+
+        string query1 =
+            "CREATE TABLE rental (rentalDate VARCHAR(255) NOT NULL, returnDate VARCHAR(255) NOT NULL, price DOUBLE NOT NULL, totalCost DOUBLE NULL);"
+        ;
+        
+        MySqlCommand command0 = new MySqlCommand(query0, connection);   
+        command0.ExecuteNonQuery();
+            
+        MySqlCommand command1 = new MySqlCommand(query1, connection);   
+        command1.ExecuteNonQuery();
+
+        int i = 0;
+        foreach (var r in rentals)
+        {
+            i++;
+            string query2 =
+                $"INSERT INTO rental VALUES({i}, '{r.rentalDate}', '{r.returnDate}', {r.price}, {r.totalCost})"
+            ;
+            
+            MySqlCommand command2 = new MySqlCommand(query2, connection);   
+            command2.ExecuteNonQuery();
         }
     }
 }
