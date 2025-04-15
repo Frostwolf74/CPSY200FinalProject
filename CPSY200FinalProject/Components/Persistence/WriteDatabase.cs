f2bd4fb46879734b2330da1b980a78851878fce9
Frost <152447084+Frostwolf74@users.noreply.github.com>
2025-04-14 20:11:44 -0600

    added database writer

--- CPSY200FinalProject/Components/Persistence/WriteDatabase.cs
+++ CPSY200FinalProject/Components/Persistence/WriteDatabase.cs
@@ -1,7 +1,31 @@
-﻿namespace CPSY200FinalProject.Components.Persistence;
+﻿using CPSY200FinalProject.Components.Layout;
+
+namespace CPSY200FinalProject.Components.Persistence;
 using MySqlConnector;
 
 public class WriteDatabase
 {
+    public static void write(List<CustomerObj> customers)
+    {
+        MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder
+        {
+            Server = "localhost",
+            Database = "CPSY200FinalProject",
+            UserID = "root",
+            Password = "password"
+        };
     
+        MySqlConnection connection = new MySqlConnection(builder.ToString());
+        connection.Open();
+
+        foreach (var customer in customers)
+        {
+            string query =
+                $"INSERT INTO customers VALUES('{customer.firstName}', '{customer.lastName}', {customer.phoneNumber}, '{customer.email}', '{customer.notes}')"
+            ;
+        
+            MySqlCommand command = new MySqlCommand(query, connection);   
+            command.ExecuteNonQuery();
+        }
+    }
 }
